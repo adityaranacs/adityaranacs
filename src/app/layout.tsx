164c89dd3ea060379f0d9bbd8b91505070d4a3d0
Aditya rana <94234121+adityaranacs@users.noreply.github.com>
2025-11-18 16:01:56 +0530

    Add Vercel Analytics component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ import EasterEggs from "@/components/easter-eggs";
 import { config } from "@/data/config";
 import SocketContextProvider from "@/contexts/socketio";
 import RemoteCursors from "@/components/realtime/remote-cursors";
+import { Analytics } from '@vercel/analytics/next';
+
 
 export const metadata: Metadata = {
   title: config.title,
@@ -88,6 +90,7 @@ export default function RootLayout({
               <TooltipProvider>
                 <Header />
                 {children}
+                <Analytics />
                 <Footer />
               </TooltipProvider>
             </SocketContextProvider>
